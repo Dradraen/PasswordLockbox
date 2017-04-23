cb21394587542caa5d8e3c265c5edcadc4524c45
Jordan Nazario <Jordan@Jordans-MacBook-Pro-2.local>
2017-04-23 15:41:27 -0500

    Fixes and input validation to password recovery

--- app/src/main/java/com/example/deric/passwordlockbox/passwordRecoverActivity.java
+++ app/src/main/java/com/example/deric/passwordlockbox/passwordRecoverActivity.java
@@ -57,6 +57,18 @@ public class passwordRecoverActivity extends AppCompatActivity {
                     });
             alertDialog.show();
         }
+        else if (pass.equals("")){
+            AlertDialog alertDialog = new AlertDialog.Builder(passwordRecoverActivity.this).create();
+            alertDialog.setMessage("You do not have a password for this domain");
+            alertDialog.setTitle("Alert");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            alertDialog.show();
+            fPassText.setText("Password will appear here");
+        }
         else if (!c.validate(master, masterPass)){
             AlertDialog alertDialog = new AlertDialog.Builder(passwordRecoverActivity.this).create();
             alertDialog.setMessage("Master password incorrect");
